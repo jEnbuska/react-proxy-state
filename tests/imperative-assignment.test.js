a08fe4ab39f3815733467cfedef7eaaee1eb961e
jenbuska <joona.enbuska@eficode.com>
2017-11-26 19:53:10 +0200

    rename tests

--- tests/imperative-assignment.test.js
+++ tests/imperative-assignment.test.js
@@ -1,35 +1,35 @@
 import change from '../src';
 
-describe('set', () => {
-    test('set object state to object state', () => {
+describe('imperative-assignment', () => {
+    test('imperative-assign object state to object state', () => {
         const subject = change({val: 1});
         subject.val = {a: 1};
         expect(subject.val.state).toEqual({a: 1});
         expect(subject.state).toEqual(({val: {a: 1}}));
     });
 
-    test('set primitive state', () => {
+    test('imperative-assign primitive state', () => {
         const subject = change({val: 1});
         subject.val = 2;
         expect(subject.val.state).toEqual(2);
         expect(subject.state).toEqual({val: 2});
     });
 
-    test('set object state to primitive', () => {
+    test('imperative-assign object state to primitive', () => {
         const subject = change({val: {a: 1}});
         subject.val = 2;
         expect(subject.val.state).toEqual(2);
         expect(subject.state).toEqual({val: 2});
     });
 
-    test('set primitive state to object', () => {
+    test('imperative-assign primitive state to object', () => {
         const subject = change({val: 1});
         subject.val = {a: 1};
         expect(subject.val.state).toEqual({a: 1});
         expect(subject.state).toEqual({val: {a: 1}});
     });
 
-    test('set none to object state', () => {
+    test('imperative-assign none to object state', () => {
         const subject = change({a: 1});
         subject.b = {c: 2};
         expect(subject.b.c.state).toEqual(2);
