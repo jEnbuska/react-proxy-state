559bfe4f84eafa5c967fcc4b5f51ff2c70841c08
jenbuska <joona.enbuska@eficode.com>
2017-12-27 01:22:42 +0200

    move proxy handler into Branch file

--- src/immutability/Branch.js
+++ src/immutability/Branch.js
@@ -5,22 +5,19 @@ import {
     eventTypes,
     valueIsAssignable
 } from '../common';
-
-import handler from './proxyHandler';
 import {sendRequest} from './createStateStore';
 
-const {IDENTITY, PROXY_CONSTRUCTOR} = branchPrivates;
-const {RESOLVE_LOCATION, RESOLVE_STATE, BRANCH_PROXY} = identityPrivates;
+const {IDENTITY, STATE} = branchPrivates;
+const {ADD, RESOLVE_LOCATION, RESOLVE_STATE, BRANCH_PROXY} = identityPrivates;
+const descriptor = {configurable: true, enumerable: true};
 const {ASSIGN, CLEAR, REMOVE, TOGGLE} = eventTypes;
 
 export default class Branch {
 
     [IDENTITY];
 
-    static [PROXY_CONSTRUCTOR](identity) {
-        const branch = new Branch();
-        branch[IDENTITY] = identity;
-        return identity[BRANCH_PROXY] = new Proxy(branch, handler);
+    constructor(identity) {
+        this[IDENTITY] = identity;
     }
 
     get state() {
@@ -52,15 +49,17 @@ export default class Branch {
         let index = 0;
         return {
             next() {
-                return {value: values[index], done: index++ === values.length}
+                return {value: values[index], done: index++ === values.length};
             },
         };
     }
 
+    // eslint-disable-next-line class-methods-use-this
     [util.inspect.custom]() {
         return '[object Branch]';
     }
 
+    // eslint-disable-next-line class-methods-use-this
     toString() {
         return '[object Branch]';
     }
@@ -77,3 +76,50 @@ export default class Branch {
         return state;
     }
 }
+
+export const handler = {
+    sendRequest: undefined,
+    get(target, property, proxy) {
+        if (property in target) {
+            return Reflect.get(target, property, proxy);
+        }
+        const identity = target[IDENTITY];
+        if (identity[property]) return identity[property][BRANCH_PROXY];
+        const state = identity[RESOLVE_STATE]();
+        if (state && state[property] !== undefined) {
+            const childIdentity = identity[ADD](property);
+            const branch = new Proxy(new Branch(childIdentity), this);
+            childIdentity[BRANCH_PROXY] = branch;
+            return branch;
+        }
+        return undefined;
+    },
+
+    set(target, property, value) {
+        if (property === STATE) {
+            target[STATE] = value;
+        } else {
+            const location = target[IDENTITY][RESOLVE_LOCATION]();
+            sendRequest(ASSIGN, location, {[property]: value && value.state ? value.state : value});
+        }
+        return true;
+    },
+
+    getOwnPropertyDescriptor() {
+        return descriptor;
+    },
+
+    has(target, prop) {
+        const state = target[IDENTITY][RESOLVE_STATE]();
+        return !!(state && state[prop] !== undefined);
+    },
+
+    ownKeys(target) {
+        //const func = Reflect.get(target, KEYS);
+        //return Reflect.apply(func, target, []);
+        const state = target[IDENTITY][RESOLVE_STATE]();
+        if (valueIsAssignable(state)) {
+            return Object.keys(state).filter(it => it !== undefined);
+        }
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import {branchPrivates, emptyFunction, identityPrivates} from './common';
-import Branch from './immutability/Branch';
+import {emptyFunction, identityPrivates} from './common';
+import Branch, {handler} from './immutability/Branch';
 import Identity from './immutability/Identity';
 import createStateStore from './immutability/createStateStore';
-import handler from './immutability/proxyHandler';
 
 const {STATE} = identityPrivates;
-const {IDENTITY} = branchPrivates;
 
 export default function createStateProxy(state, onChange) {
-    const root = new Branch();
     const identity = new Identity(undefined, undefined, 0);
     identity[STATE] = state;
     createStateStore(identity, onChange || emptyFunction, identity);
-    root[IDENTITY] = identity;
-    return new Proxy(root, handler);
+    return new Proxy(new Branch(identity), handler)
 }
 
 export {default as createProvider} from './CreateProvider';
